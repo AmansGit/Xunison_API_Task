29e9c941a846c3311e84384c3fbe2cdd9d627eeb
Aman Sharma <amansharma@decoderslabs.co>
2021-05-29 22:01:38 +0530

    test_model added

--- movie_crud/tests/test_models.py
+++ movie_crud/tests/test_models.py
@@ -6,8 +6,6 @@ from datetime import datetime
 
 class MovieContainerTestCase(TestCase):
 	def setUp(self):
-		# print("hii")
-		# dor = datetime.date("2020-12-03")
 		dor1 = datetime.strptime("2020-12-03", '%Y-%m-%d')
 		dor2 = datetime.strptime("2021-05-14", '%Y-%m-%d')
 		dor3 = datetime.strptime("2007-12-05", '%Y-%m-%d')
@@ -16,19 +14,27 @@ class MovieContainerTestCase(TestCase):
 		MovieContainer.objects.create(name="Army of the Dead", description="After a zombie outbreak in Las Vegas, a group of mercenaries takes the ultimate gamble by venturing into the quarantine zone for the greatest heist ever.", date_of_release=dor2)
 		MovieContainer.objects.create(name="I Am Legend", description="Robert Neville, a scientist, is the last human survivor of a plague in the whole of New York. ", date_of_release=dor3)
 
-		# c_movie.save()
-
-
 	def test_get_movie_with_id_pass(self):
-		# print("HIII")
 		movie_obj = MovieContainer.objects.get(id=1)
 		self.assertEqual(movie_obj.name, "The Mummy")
 		self.assertTrue(movie_obj.description, "This is a Horror movie")
 
-	def test_get_movie_with_id_fail(self):
-		movie_obj = MovieContainer.objects.filter(id=4)
-		# self.assertEqual(movie_obj.name, "The Mummy")
-		self.assertFalse(False)
-	# def test_get_movies_details(self):
-	# 	movie_obj = MovieContainer.objects.all()
+	# def test_get_movie_with_id_fail(self):
+	# 	movie_obj = MovieContainer.objects.get(id=4)
+	# 	self.assertFalse(False)
+	
+# ---- 3 records are there in default db
+	def test_get_movies_details(self):
+		movie_obj = MovieContainer.objects.all()
+		self.assertEqual(movie_obj.count(), 3)
 
+	
+	def test_update_movies_with_id(self):
+		MovieContainer.objects.filter(id=1).update(name="Mummy horror movie", description="Horror movie")
+		self.assertTrue(True, "this is false?")
+
+
+	def test_delete_movie_with_id(self):
+		movie_obj = MovieContainer.objects.get(id=1)
+		movie_obj.delete()
+		self.assertTrue(True, "not deleted")
